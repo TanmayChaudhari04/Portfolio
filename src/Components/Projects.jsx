82ae77acbf7ab45605d9ed31fa9675dd74b4ef0d
TanmayChaudhari04 <tanmayschaudhari@gmail.com>
2024-07-27 12:09:33 -0700

    changes in project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,6 +8,8 @@ import DoIt from '../assets/DoIt.png';
 import WordFreq from '../assets/WordFreq.png';
 import ProceduralIsland from '../assets/Island.png';
 import ProjectCards from "./ProjectCards";
+import WalletWhiz from "../assets/FinanceTracker.png";
+import Movie from "../assets/Movie.png";
 
 function Projects(){
     
@@ -26,6 +28,13 @@ function Projects(){
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-16">
                 <div className="grid gap-4">
                     <div>
+                        <ProjectCards title="Wallet Whiz"
+                        content="Wallet Whiz is a personal finance tracker that enables users to log in with their Google account and manage their finances securely. Users will gain better control over their financial decisions by visualizing their spending habits through categorized charts, tracking income over time with a dynamic line chart, and reviewing all transactions in a detailed table."
+                        url={WalletWhiz}
+                        githubLinks="https://github.com/TanmayChaudhari04/WalletWhiz">
+                        </ProjectCards>
+                    </div>
+                    <div>
                         <ProjectCards title="WeatherNow"
                         content="Developed a web application using JavaScript, HTML, and CSS to provide real-time weather updates for all cities around the world."
                         url={WeatherNow}
@@ -35,10 +44,10 @@ function Projects(){
                 </div>
                 <div className="grid gap-4">
                     <div>
-                        <ProjectCards title="Do-It"
-                        content="Developed a To-Do List web application using JavaScript, HTML, and CSS that helps you manage your task with ease and it also stores your tasks."
-                        url={DoIt}
-                        githubLinks="https://github.com/TanmayChaudhari04/Do-It">
+                        <ProjectCards title="Movie Recommendation System"
+                        content="Developed a movie recommendation system using the Python and Pandas and MovieLens 25M dataset, which includes millions of user ratings and reviews. The system allows users to input a movie title and receive recommendations for other movies they might enjoy."
+                        url={Movie}
+                        githubLinks="https://github.com/TanmayChaudhari04/Movie-Recommendation-System">
                         </ProjectCards>
                     </div>
                     <div>
@@ -50,11 +59,18 @@ function Projects(){
                     </div>
                 </div>
                 <div className="grid gap-4">
-                    <div>
+                     <div>
                         <ProjectCards title="Word Frequency Analysis"
                         content="Developed and implemented a text parsing algorithm in C++ by using binary search tree to analyze a large text file, producing an exact word frequency count for each word in the file."
                         url={WordFreq}
                         githubLinks="https://github.com/TanmayChaudhari04/Word-Frequency-Analysis">
+                        </ProjectCards>
+                    </div>
+                    <div>
+                        <ProjectCards title="Do-It"
+                        content="Do-it is a simple yet powerful To-Do List application designed to help you manage your tasks with ease. Built with JavaScript, HTML, and CSS, this web app ensures that your tasks are saved even if you close the tab, providing a seamless user experience. Whether you're organizing your daily schedule or planning a long-term project, Do-it offers a user-friendly interface to keep you on track."
+                        url={DoIt}
+                        githubLinks="https://github.com/TanmayChaudhari04/Do-It">
                         </ProjectCards>
                     </div>
                 </div>
