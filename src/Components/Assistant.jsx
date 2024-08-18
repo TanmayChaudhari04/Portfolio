effcac61df2b55e9bb8d78ddec21809626dc82e6
TanmayChaudhari04 <tanmayschaudhari@gmail.com>
2024-08-18 10:39:38 -0700

    added AI Assistant

--- src/Components/Assistant.jsx
+++ src/Components/Assistant.jsx
@@ -116,10 +116,11 @@ function Assistant() {
                 <div className="mt-6 text-2xl text-center text-white font-semibold">
                     How can I help you today?
                 </div>
+                <div className="flex justify-center items-center mt-6 flex-wrap">
                 <input
                     id="chat"
                     rows="1"
-                    className="ml-80 p-2.5 w-6/12 text-sm text-gray-900 bg-gray-700 rounded-lg border border-gray-600 focus:ring-0 focus:border-gray-600 dark:bg-gray-800 dark:border-gray-800 dark:placeholder-gray-500 dark:text-white dark:focus:ring-0 dark:focus:border-gray-600"
+                    className="flex-grow sm:flex-grow-0 w-full sm:w-10/12 md:w-6/12 max-w-xs sm:max-w-md md:max-w-lg p-2.5 text-sm text-gray-900 bg-gray-700 rounded-lg border border-gray-600 focus:ring-0 focus:border-gray-600 dark:bg-gray-800 dark:border-gray-800 dark:placeholder-gray-500 dark:text-white dark:focus:ring-0 dark:focus:border-gray-600"
                     placeholder="Ask me anything about Tanmay ..."
                     value={requestText}
                     onChange={(e) => setRequestText(e.target.value)}
@@ -128,15 +129,16 @@ function Assistant() {
                 />
                 <button
                     type="button"
-                    className="mt-6 ml-6 text-white bg-sky-700 hover:bg-sky-100 focus:ring-4 focus:outline-none focus:ring-sky-200 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-sky-800 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
+                    className="ml-2 text-white bg-sky-700 hover:bg-sky-100 focus:ring-4 focus:outline-none focus:ring-sky-200 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:bg-sky-800 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
                     onClick={handleSendMessage}
                 >
-                <svg className="w-5 h-4 my-1 mx-1 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
-                </svg>
-                <span className="sr-only">Send message</span>
+                    <svg className="w-5 h-4 my-1 mx-1 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                        <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
+                    </svg>
+                    <span className="sr-only">Send message</span>
                 </button>
-                <div className="text-center text-white mt-9 text-xl mx-28" dangerouslySetInnerHTML={{ __html: replyText }} />
+                </div>
+                <div className="text-center text-white mt-9 text-xl mx-28 lg:text-xl sm:px-16 lg:px-48" dangerouslySetInnerHTML={{ __html: replyText }} />
                 <div className="my-12 justify-center flex space-x-4">
                     <button className="text-black rounded bg-sky-500 py-2 px-8 hover:bg-sky-400 font-bold border-b-4 border-blue-700 hover:border-sky-500">
                         <a href={tResume} target={"_blank"}>Résumé</a>
